b9b3835d240ba04101b5970e414154e401c43b78
Subash Maharjan <hudeoworks@gmail.com>
2025-08-12 10:40:36 -0400

    Update theme.ts

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -2,6 +2,7 @@
 'use client';
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
+import type {} from '@mui/material/themeCssVarsAugmentation';
 
 const roboto = Roboto({
     weight: ['300', '400', '500', '700'],
@@ -13,6 +14,10 @@ const theme = createTheme({
     typography: {
         fontFamily: roboto.style.fontFamily,
     },
+    colorSchemes: { light: true, dark: true },
+    cssVariables: {
+        colorSchemeSelector: 'class'
+    }
 });
 
 export default theme;
